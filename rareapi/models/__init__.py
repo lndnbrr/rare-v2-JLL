93c0a3a1f532ef7851cbd69f038307327bc49d94
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-06-10 20:09:44 -0500

    feat: added create function for comments, comment migration file

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -1,3 +1,4 @@
-from .category import   Category
+from .category import Category
 from .post import Post
 from .user import User
+from .comment import Comment
---
+++ rareapi/views/comment.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import Comment, Post, User
+
+
+class CommentView(ViewSet):
+    """Rare comments view"""
+
+    def create(self, request):
+        """Handle POST requests for comments
+
+        Returns JSON serialized comment instance
+        """
+        author = User.objects.get(pk=request.data["author"])
+        post = Post.objects.get(pk=request.data["post"])
+
+        comment = Comment.objects.create(
+            content=request.data["content"],
+            created_on=request.data["created_on"],
+            author=author,
+            post=post
+        )
+
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """JSON serializer for comments"""
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'content', 'created_on')
+        depth = 1
