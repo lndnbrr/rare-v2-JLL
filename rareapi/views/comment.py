726a8e46137bdcc286dac3595fb70950df1c6a62
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-06-17 21:10:13 -0500

    feat: edit and delete comments, working with frontend

--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -55,19 +55,13 @@ class CommentView(ViewSet):
         comment.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
-    def update(self, request, pk):
+    def partial_update(self, request, pk):
         """Handle PUT requests for a comment
 
         Returns: Response - empty body with 204 status code
         """
         comment = Comment.objects.get(pk=pk)
         comment.content = request.data["content"]
-        comment.created_on = request.data["created_on"]
-
-        author = User.objects.get(pk=request.data["author"])
-        post = Post.objects.get(pk=request.data["post"])
-        comment.author = author
-        comment.post = post
 
         comment.save()
 
